c34f42e3cdcda07db41b679f14b52662ccc9dd86
Andon Mitev <mitevandon94@gmail.com>
2024-02-06 09:39:35 +0200

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,34 @@
+import { PuppyImages } from '@/lib/constants';
+import { getFrameMetadata } from '@coinbase/onchainkit';
+import type { Metadata } from 'next';
+
+const frameMetadata = getFrameMetadata({
+  buttons: [
+    {
+      label: 'Begin'
+    }
+  ],
+  image: PuppyImages.first,
+  post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=1`
+});
+
+export const metadata: Metadata = {
+  title: 'Donnie Testing Frames',
+  description: 'A frame where bugs happens wrapped in magic',
+  openGraph: {
+    title: 'Donnie Testing Frames',
+    description: 'A frame where bugs happens wrapped in magic',
+    images: [PuppyImages.first]
+  },
+  other: {
+    ...frameMetadata
+  }
+};
+
+export default function Page() {
+  return (
+    <>
+      <h1>Donnie Testing Frames</h1>
+    </>
+  );
+}
---
+++ lib/constants.ts
@@ -0,0 +1,8 @@
+export const PuppyImages = {
+  first:
+    'https://img.freepik.com/free-photo/puppy-that-is-walking-snow_1340-37228.jpg?size=626&ext=jpg&ga=GA1.1.34264412.1707177600&semt=sph',
+  second:
+    'https://thumbs.dreamstime.com/b/happy-little-havanese-puppy-sitting-grass-orange-dog-45886384.jpg',
+  third:
+    'https://media.istockphoto.com/id/611308904/photo/cute-sitting-havanese-puppy-dog.jpg?s=612x612&w=0&k=20&c=OUmBXgww0CGOStMzapqVpxOwbla-sZDaS2DpKCjTQrk='
+};
